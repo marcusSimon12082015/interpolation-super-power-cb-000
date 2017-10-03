cec12f1b93da29118d66143ca7e2a3e3b5577a43
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-03 19:48:26 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,12 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  niz = ""
+	colors.each_with_index do |element,index|
+		if index < colors.size - 1
+    			niz += "#{element[0].upcase}: #{element}, "
+		else
+    			niz += "#{element[0].upcase}: #{element}"
+		end
+	end
+  puts niz
+end
